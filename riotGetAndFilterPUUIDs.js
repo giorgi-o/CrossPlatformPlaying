5ed2e5102afdd2f467307640368939a2969ac99e
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2021-09-26 16:30:18 +0100

    fix parsing to usersMap with multiple users

--- riotGetAndFilterPUUIDs.js
+++ riotGetAndFilterPUUIDs.js
@@ -2,7 +2,7 @@
 // and nicely format the usernames and PUUIDs.
 
 // put your cookies here:
-const riotCookies = "";
+const riotCookies = "__cf_bm=ot4TcGN65O4xMv.NSkyYBF_iPrWZC8F63MdMgPrE75w-1632660927-0-AWQz5tyXmB5S+bDOwX407HwUdgv8UdR79vKlDYsNkHUspDlux2wrdff/xfr/9AmiWRueQKAoSDCqH4WcaR4vy60=; tdid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg4MzI1MTBlLWI5MjItNDQ3MC05YjljLTRhMTU0N2EwZTFkMyIsIm5vbmNlIjoiOXF3ZzRCVmJWMEE9IiwiaWF0IjoxNjMyNjYwOTQ0fQ.dU-7otayjhzjGQwGm6PctVcJXVA4Z6LKkyMgYB6OlzI; clid=ec1; sub=1c98a66c-621b-56fc-aff0-4f6d5404af45; csid=aqAwAFZR7WFROOHQcyRQug.uac9E7rkKybjgICnRXHhhg; ssid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzc2lkIjoiYXFBd0FGWlI3V0ZST09IUWN5UlF1Zy51YWM5RTdya0t5YmpnSUNuUlhIaGhnIiwic3ViIjoiMWM5OGE2NmMtNjIxYi01NmZjLWFmZjAtNGY2ZDU0MDRhZjQ1IiwibG9naW5Ub2tlbiI6ImFmMjQ5OWJhLTRkMGItNDJhYi1iN2E3LTQ3MDA1ODMzNTEwYSIsInNlcmllc1Rva2VuIjoiODViYWVhZmYtYzJjMy00YTg2LTgyZGMtMzA2NmI2Y2ZlZTJhIiwiaWF0IjoxNjMyNjYwOTQ1fQ.yQM8zuZVnSZYybH755PW-LXnYgmb-ZOEkgBhBtfMqqA; PVPNET_TOKEN_EUW=eyJkYXRlX3RpbWUiOjE2MzI2NjA5NDY0MDEsImdhc19hY2NvdW50X2lkIjoyMzI0MTM1NjQsInB2cG5ldF9hY2NvdW50X2lkIjoyMzI0MTM1NjQsInN1bW1vbmVyX25hbWUiOiJHaW9yZ2lvY2F2MSIsInZvdWNoaW5nX2tleV9pZCI6IjkwMzQ3NTJiMmI0NTYwNDRhZTg3ZjI1OTgyZGFkMDdkIiwic2lnbmF0dXJlIjoiSnNpNlZYM2ExK1RRWUJ5UkM5RXJFWDVSNFFzVHFCZkVVdVYyWjBuZDhFaFVPWHZBdElwOVJmdVRjTktsZVJVWFZ1Y09VNGlNZW9hMWVSV2kxb0gyYmtXWmlnWGcreVZIdjI5ZllTL1pXWGxqTUVreHh0b2JjRWhzbk1pU0taQjQyVThQODVIbXZtYUs1cVUxNGpkc2NEUnZJeTVqVjc3SjVxVldTY0RGY25RPSJ9; PVPNET_ACCT_EUW=Giorgiocav1; PVPNET_ID_EUW=232413564; PVPNET_REGION=euw; PVPNET_LANG=en_US; id_token=eyJraWQiOiJzMSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxYzk4YTY2Yy02MjFiLTU2ZmMtYWZmMC00ZjZkNTQwNGFmNDUiLCJjb3VudHJ5IjoicnVzIiwicGxheWVyX3Bsb2NhbGUiOiJlbi1VUyIsImNvdW50cnlfYXQiOjE2Mjk3MTA5NDUwMDAsImFtciI6WyJwYXNzd29yZCIsIm1mYSJdLCJpc3MiOiJodHRwczpcL1wvYXV0aC5yaW90Z2FtZXMuY29tIiwibG9sIjpbeyJ1aWQiOjIzMjQxMzU2NCwiY3VpZCI6MjMyNDEzNTY0LCJ1bmFtZSI6Imdpb3JnaW9jYXYxIiwiY3BpZCI6IkVVVzEiLCJwdHJpZCI6bnVsbCwicGlkIjoiRVVXMSIsInN0YXRlIjoiRU5BQkxFRCJ9XSwibG9jYWxlIjoiZW5fVVMiLCJhdWQiOiJyc28td2ViLWNsaWVudC1wcm9kIiwiYWNyIjoidXJuOnJpb3Q6Z29sZCIsInBsYXllcl9sb2NhbGUiOiJlbi1VUyIsImV4cCI6MTYzMjc0NzM0NSwiaWF0IjoxNjMyNjYwOTQ1LCJhY2N0Ijp7ImdhbWVfbmFtZSI6Ikdpb3JnaW8iLCJ0YWdfbGluZSI6IjAwMCJ9LCJhZ2UiOjE4LCJqdGkiOiJJdVUzN0NvcDZVYyIsImxvZ2luX2NvdW50cnkiOiJnYnIifQ.WbPX-ndg5n31rwaSZKd3dRSYohAB9BtIQ6EAlc77RI-t70ftP45MkmL7G-LRiXAr2qvkM-5h8VAlxilg2HyvrDAy8xsJ0c9Ma-jIpBfQEuvHCPjR5Ir9FplZD2cPGwxLVSRkRUs8NNWTfTt1UAwO_pVPtyn9THMTfDKJtvfTc2Y; id_hint=sub%3D1c98a66c-621b-56fc-aff0-4f6d5404af45%26lang%3Den%26game_name%3DGiorgio%26tag_line%3D000%26id%3D232413564%26summoner%3DGiorgiocav1%26region%3DEUW1%26tag%3Deuw";
 
 const https = require("https")
 const tls = require("tls");
@@ -157,15 +157,20 @@ function riotEstablishXMPPConnection(RSO, PAS) {
                     puuidToUsername[puuid] = `${username} #${tagline}`;
                 }
 
-                const puuidToDiscordID = {};
+                const discordIdToPUUID = {};
                 console.log("=========================================");
 
                 for(const puuid in puuidToUsername) {
                     const discord_id = await getDiscordID(puuidToUsername[puuid]);
-                    if(discord_id) puuidToDiscordID[discord_id] = puuid;
+                    if(discord_id) {
+                        if(discordIdToPUUID[discord_id])
+                            discordIdToPUUID[discord_id].push(puuid);
+                        else
+                            discordIdToPUUID[discord_id] = [puuid];
+                    }
                 }
 
-                console.log('"usersMap": ' + JSON.stringify(puuidToDiscordID, null, '    '));
+                console.log('"usersMap": ' + JSON.stringify(discordIdToPUUID, null, '    '));
 
                 data = "";
             } else sendNext();
